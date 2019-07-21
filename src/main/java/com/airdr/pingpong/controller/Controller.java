cb41fb5825cac4a1ffbf9ef8a3d19b270d710770
aleksreingand <aleksreingand@192.168.0.41>
2019-07-21 18:14:03 +0300

    updated controller

--- src/main/java/com/airdr/pingpong/controller/Controller.java
+++ src/main/java/com/airdr/pingpong/controller/Controller.java
@@ -2,10 +2,16 @@ package com.airdr.pingpong.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.airdr.pingpong.api.Api;
+import com.airdr.pingpong.api.ResponseServer;
 import com.airdr.pingpong.entities.Participant;
 import com.airdr.pingpong.repository.IParticipant;
 
@@ -21,5 +27,23 @@ public class Controller {
 	public Iterable<Participant> createParticipant()
 	{
 		return participant.getAllParticipant();
+	}
+	
+	@PostMapping(Api.PARTICIPANT)
+	public ResponseServer newParticipant(@RequestBody Participant entity)
+	{
+		return participant.addParticipant(entity);
+	}
+	
+	@PutMapping(Api.PARTICIPANT)
+	public ResponseServer updateParticipant(@RequestParam(name = Api.DATA) int id, @RequestParam Participant entity)
+	{
+		return participant.updateInfo(id, entity);
+	}
+	
+	@DeleteMapping(Api.PARTICIPANT)
+	public ResponseServer deleteParticipant(@RequestParam(name = Api.DATA) int id)
+	{
+		return participant.removeParticipant(id);
 	}
 }
